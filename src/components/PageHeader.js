5e689aad798601a710c2c10cb339e3fb06808cc8
Eric Lee <szl0094@auburn.edu>
2023-07-23 13:29:09 -0500

    init

---
+++ src/components/PageHeader.js
@@ -0,0 +1,12 @@
+import "./PageHeader.css";
+
+function PageHeader(props) {
+  return (
+    <div id="pageHeaderContainer">
+      <div className="headerText">Country information project</div>
+      <div className="divider"></div>
+    </div>
+  );
+}
+
+export default PageHeader;
